354c2caddd419fe8dce93aa494963396786202ca
Elton Horacio <cantinfras2@gmail.com>
2018-11-26 07:19:52 -0200

    Inicio do Projeto

---
+++ src/main/java/com/eltonhoracio/carteiradourada/CarteiradouradaApplication.java
@@ -0,0 +1,12 @@
+package com.eltonhoracio.carteiradourada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiradouradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiradouradaApplication.class, args);
+	}
+}
