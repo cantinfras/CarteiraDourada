78c6603a6ec04cfc2bd32f95ddbb7d6a2395fb68
Elton Horacio <cantinfras2@gmail.com>
2018-12-02 11:36:09 -0200

    Criando Instanciação TipoMulta

--- src/main/java/com/eltonhoracio/carteiradourada/CarteiradouradaApplication.java
+++ src/main/java/com/eltonhoracio/carteiradourada/CarteiradouradaApplication.java
@@ -1,12 +1,32 @@
 package com.eltonhoracio.carteiradourada;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.eltonhoracio.carteiradourada.domain.TipoMulta;
+import com.eltonhoracio.carteiradourada.repositories.TipoMultaRepository;
+
 @SpringBootApplication
-public class CarteiradouradaApplication {
+public class CarteiradouradaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private TipoMultaRepository tipoMultaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarteiradouradaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		TipoMulta tipo1 = new TipoMulta(null, "501-00", "DIRIGIR VEICULO SEM POSSUIR CNH PPD ACC", 7);
+		TipoMulta tipo2 = new TipoMulta(null, "502-91", "DIRIGIR VEICULO COM CNH PPD ACC CASSADA", 7);
+		
+		tipoMultaRepository.saveAll(Arrays.asList(tipo1, tipo2));
+		
 	}
 }
