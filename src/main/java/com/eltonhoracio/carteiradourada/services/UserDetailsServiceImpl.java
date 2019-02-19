ecdcf2befdc8b27d090de1e0003adb0a27f56062
Elton Horacio <cantinfras2@gmail.com>
2019-02-19 07:48:13 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/eltonhoracio/carteiradourada/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.eltonhoracio.carteiradourada.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.eltonhoracio.carteiradourada.domain.Pessoa;
+import com.eltonhoracio.carteiradourada.repositories.PessoaRepository;
+import com.eltonhoracio.carteiradourada.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private PessoaRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Pessoa pes = repo.findByEmail(email);
+		if(pes == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(pes.getId(), pes.getEmail(), pes.getSenha(), pes.getPerfis());
+	}
+
+}
